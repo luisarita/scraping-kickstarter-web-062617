c176bd2afdc583e6ba3226db9a9ee137fb025c32
Luis Arita <github@luisarita.com>
2017-07-06 11:35:49 -0400

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,27 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'open-uri'
+
+
+  def get_page
+    html = open("./fixtures/kickstarter.html")
+    doc = Nokogiri::HTML(html)    
+  end
+  def get_projects
+    doc = get_page
+    doc.css(".project-card")
+  end
 
 def create_project_hash
+  hash = {}
+  get_projects.each do |project|
+    title = project.css(".bbcard_name").css("strong").css("a").text
+    hash[title] = {
+      :image_link => project.css(".project-thumbnail").css("a").css(".projectphoto-little").css("img")[0].attributes["src"].value, #.text,
+      :description => project.css(".bbcard_blurb").text,
+      :location => project.css(".project-stats").css("li")[0].css("strong").text.gsub("%", ""),
+      :percent_funded => 10
+    }
+  end
   # write your code here
+  hash
 end
